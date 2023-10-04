17657b20603f982b96c76b4f05e982ac32d13fff
Abdullah <dola.doit@gmail.com>
2023-10-04 12:51:12 +0700

    finish migration and make role on middleware

---
+++ database/migrations/2023_10_03_034136_create_sales_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('qty');
+            $table->unsignedInteger('price');
+            $table->unsignedBigInteger('sales_id');
+            $table->foreign('sales_id')->references('id')->on('sales');
+            $table->unsignedBigInteger('inventory_id');
+            $table->foreign('invetory_id')->references('id')->on('inventories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales_details');
+    }
+};
--- database/migrations/2023_10_03_034903_create_purchase_details_table.php
+++ database/migrations/2023_10_03_034903_create_purchase_details_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('purchase_details', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->unsignedTinyInteger('role');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('purchase_id');
+            $table->foreign('purchase_id')->references('id')->on('purchase');
+            $table->unsignedBigInteger('inventory_id');
+            $table->foreign('invetory_id')->references('id')->on('inventories');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('purchase_details');
     }
 };
